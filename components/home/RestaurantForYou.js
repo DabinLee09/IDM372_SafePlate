45308e4bdfd30a1d034ebe241e3b9577fc698d29
gmaaddi <grm76@drexel.edu>
2024-03-18 08:50:07 -0400

    Style RestaurantForYou.js
    Style cards, add spacing and pills, etc.

--- components/home/RestaurantForYou.js
+++ components/home/RestaurantForYou.js
@@ -8,8 +8,12 @@ import {
   ScrollView,
 } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
-import { PrimaryColors } from "../../settings/styles/Colors";
+import { PrimaryColors, SecondaryColors, TintsColors } from "../../settings/styles/Colors";
 import { useData } from "../DataContext";
+import { spacing } from "../../settings/styles/Spacing";
+import Pill from "../../settings/Pill";
+import DietaryIcons from "../../assets/DietaryIcons";
+import GlobalStyles from "../../settings/styles/GlobalStyle";
 
 const YELP_API_KEY =
   "DaWbOrSWK54qhbQSXVUzcYTsEunAKnfwJ3i4NA8F4oVSSbUdvK5aDF4_xKpSh7fCO63rAVHvuQqEk6V7R3mTFg0y9Q5MiC4hSGEs7AdsE88FTfM7NFlzL73weZQ3ZXYx";
@@ -71,8 +75,8 @@ export default function RestaurantForYou({ props }) {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <View>
         {/* Titles */}
-        <Text style={type.heading2L}>Restaurant For You</Text>
-        <Text style={type.body1M}>
+        <Text style={type.heading2L}>Restaurants For You</Text>
+        <Text style={[type.body1M, spacing.vert2x]}>
           Carefully selected based on your restrictions
         </Text>
         <ScrollView horizontal showsHorizontalScrollIndicator={false}>
@@ -129,7 +133,8 @@ const RestaurantImage = (props) => (
         uri: props.image,
       }}
       style={{
-        width: 240,
+        // width: 240,
+        width: 265,
         height: 155,
         position: "relative",
         borderTopRightRadius: 4,
@@ -139,8 +144,10 @@ const RestaurantImage = (props) => (
     <TouchableOpacity
       style={{
         position: "absolute",
-        right: 20,
-        top: 20,
+        // right: 20,
+        // top: 20,
+        right: 8,
+        top: 8,
       }}
     >
       <MaterialCommunityIcons name="heart-outline" size={25} color="#fff" />
@@ -149,33 +156,68 @@ const RestaurantImage = (props) => (
 );
 
 const RestaurantInfo = (props) => (
-  <View
-    style={{
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItem: "Center",
-      padding: 8,
-      // backgroundColor:'white',
-      borderBottomLeftRadius: 4,
-      borderBottomRightRadius: 4,
-    }}
-  >
-    <View style={{ gap: 0 }}>
+  <View style={{padding: 8,}}>
       <Text style={type.heading3M}>{props.name}</Text>
-      <View
-        style={{
-          flexDirection: "row",
-          gap: 4,
-          alignItems: "center",
-        }}
-      >
+      <View style={[spacing.vert1x, {flexDirection: 'row', gap: 4, alignItems: 'center',}]}>
         <Ionicons name="location-sharp" size={16} color={PrimaryColors.Green} />
         <Text style={type.body2S}>1 mi </Text>
         <Ionicons name="star" size={16} color={"#FECA72"} />
         <Text style={type.body2S}>{props.rating} </Text>
       </View>
+
+      <Text style={[type.heading5S, spacing.vert1x]}>Dietary-friendly options include:</Text>
+
+      <View style={{
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        maxWidth: 240,
+        }}>
+
+      {/* Pills are hard-coded right now with arbitrary names and icons assigned */}
+      <Pill
+        style={[spacing.vert1x, { marginRight: 8 }]}
+        size="small" // Specify the size: 'small' or 'large'
+        type="inactive" // Specify the type: 'active' or 'inactive'
+        dietaryType="allergy" // Specify the dietary type: 'allergy' or 'diet'
+        text="Dairy" // Specify the text to display in the pill
+        icon={DietaryIcons.DairySolid} // Pass the icon component or source if needed
+      />
+
+      <Pill
+        style={[spacing.vert1x, { marginRight: 8 }]}
+        size="small" // Specify the size: 'small' or 'large'
+        type="inactive" // Specify the type: 'active' or 'inactive'
+        dietaryType="allergy" // Specify the dietary type: 'allergy' or 'diet'
+        text="Sesame" // Specify the text to display in the pill
+        icon={DietaryIcons.SesameSolid} // Pass the icon component or source if needed
+      />
+
+      <Pill
+        style={[spacing.vert1x, { marginRight: 8 }]}
+        size="small" // Specify the size: 'small' or 'large'
+        type="inactive" // Specify the type: 'active' or 'inactive'
+        dietaryType="allergy" // Specify the dietary type: 'allergy' or 'diet'
+        text="Egg" // Specify the text to display in the pill
+        icon={DietaryIcons.EggSolid} // Pass the icon component or source if needed
+      />
+
+      <Pill
+        style={[spacing.vert1x, { marginRight: 8 }]}
+        size="small" // Specify the size: 'small' or 'large'
+        type="inactive" // Specify the type: 'active' or 'inactive'
+        dietaryType="diet" // Specify the dietary type: 'allergy' or 'diet'
+        text="Vegan" // Specify the text to display in the pill
+        icon={DietaryIcons.VeganSolid} // Pass the icon component or source if needed
+      />
+
+    {/* This is the button for the +4 or whatever the number is in the event of overflow but there is no logic applied right now*/}
+    <View style={[GlobalStyles.radius25x, { borderWidth: 1, borderColor: TintsColors.MidLightGray, width: 36, height: 36, justifyContent: 'center', alignItems: 'center' }]}>
+      <Text style={[type.body2S, { color: TintsColors.MidLightGray }]}>+4</Text>
+    </View>
+
     </View>
   </View>
+  // </View>
 );
 
 const RestaurantDietInfo = () => {
@@ -186,13 +228,13 @@ const RestaurantDietInfo = () => {
 
 const styles = StyleSheet.create({
   card: {
-    elevation: 2,
-    shadowColor: "#000",
-    shadowOpacity: 1.15,
+    backgroundColor: 'white',
+    ...GlobalStyles.radius1x,
+    shadowColor: 'rgba(0, 0, 0, 0.15)',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 1,
     shadowRadius: 4,
-    shadowOffset: {
-      width: 2,
-      height: 2,
-    },
+    elevation: 4,
+    ...spacing.vert3x
   },
 });
