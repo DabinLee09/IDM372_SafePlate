ae3d7aa8a2c5ba3df4b6cd1072024530a7327e3f
DabinLee09 <dl3277@drexel.edu>
2024-03-18 10:30:29 -0400

    update what search

--- components/searchTab/DietSelect.js
+++ components/searchTab/DietSelect.js
@@ -1,27 +1,34 @@
 import { View, Text, TouchableOpacity, FlatList } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Pill from "../../settings/Pill";
 import { useData } from "../DataContext";
 import { PrimaryColors } from "../../settings/styles/Colors";
 
-export default function AllergySelect({ selectedItems, updateSelectedItems }) {
-  const { allergyArr, } = useData();
-//   const [selectedItems, setSelectedItems] = useState([]);
+export default function AllergySelect() {
+  const { dietArr, selectedRestriction, setSelectedRestriction} = useData();
+  const [selectedItems, setSelectedItems] = useState([]);
 
   const toggleSelect = (index) => {
-    const newArr = [...allergyArr];
+    const newArr = [...dietArr];
     newArr[index].type = newArr[index].type === "active" ? "inactive" : "active";
     const newSelectedItems = newArr.filter((item) => item.type === "active");
-    updateSelectedItems(newSelectedItems);
+    setSelectedItems(newSelectedItems);
+    // updateSelectedRestriction(newSelectedItems);
   };
 
-
+  const updateSelectedRestriction = (newSelectedItems) => {
+    if (newSelectedItems) { // Ensure newSelectedItems is defined
+      setSelectedRestriction([...selectedRestriction, ...newSelectedItems]);
+    }
+  };
+  
+  
 
   return (
     <View>
       <Text>AllergySelect</Text>
       <FlatList
-        data={allergyArr}
+        data={dietArr}
         renderItem={({ item, index }) => (
           <TouchableOpacity
             key={index}
@@ -34,7 +41,7 @@ export default function AllergySelect({ selectedItems, updateSelectedItems }) {
               alignItems: "center",
               height: 48,
             }}
-            onPress={() => toggleSelect(index)}
+            onPress={() => {toggleSelect(index)}}
           >
             <Pill
               size="large"
@@ -52,6 +59,12 @@ export default function AllergySelect({ selectedItems, updateSelectedItems }) {
       <Text>Selected: {selectedItems.length}</Text>
       <Text>
         {selectedItems.map((item, index) => (
+          <Text key={index}>{item.name}, </Text>
+        ))}
+      </Text>
+      <Text>Selected: {selectedRestriction.length}</Text>
+      <Text>
+        {selectedRestriction.map((item, index) => (
           <Text key={index}>{item.name}, </Text>
         ))}
       </Text>
